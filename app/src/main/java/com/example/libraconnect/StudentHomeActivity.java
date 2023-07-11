6ea354610a4df2904cbe8b2e9594cdaaea68f942
DanielGals <janjangallaron@gmail.com>
2023-07-11 21:48:48 +0800

    Fixed Few buttons and added firebase and Auth and login

--- app/src/main/java/com/example/libraconnect/StudentHomeActivity.java
+++ app/src/main/java/com/example/libraconnect/StudentHomeActivity.java
@@ -4,10 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class StudentHomeActivity extends AppCompatActivity {
 
@@ -17,12 +27,21 @@ public class StudentHomeActivity extends AppCompatActivity {
     StudentBookmarkFragment studentBookmarkFragment = new StudentBookmarkFragment();
     StudentMailFragment studentMailFragment = new StudentMailFragment();
     StudentSettingsFragment studentSettingsFragment = new StudentSettingsFragment();
+    private FirebaseAuth mAuth;
+
+    private TextView displayUser;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        Log.d("Heys", "Name:asdasdasdddddddd");
 
         bottomNavigationView = findViewById(R.id.studentBottomNav);
 
@@ -34,24 +53,27 @@ public class StudentHomeActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
 
-                    int itemId = item.getItemId();
+                int itemId = item.getItemId();
 
-                    if (itemId == R.id.homeButton) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentHomeFragment).commit();
-                        return true;
-                    } else if (itemId == R.id.bookmarkButton) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentBookmarkFragment).commit();
-                        return true;
-                    } else if (itemId == R.id.mailButton) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentMailFragment).commit();
-                        return true;
-                    } else if (itemId == R.id.settingsButton) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentSettingsFragment).commit();
-                        return true;
-                    }
+                if (itemId == R.id.homeButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentHomeFragment).commit();
+                    return true;
+                } else if (itemId == R.id.bookmarkButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentBookmarkFragment).commit();
+                    return true;
+                } else if (itemId == R.id.mailButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentMailFragment).commit();
+                    return true;
+                } else if (itemId == R.id.settingsButton) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, studentSettingsFragment).commit();
+                    return true;
+                }
                 return false;
             }
         });
 
+
     }
+
+
 }
--- app/src/main/java/com/example/libraconnect/StudentHomeFragment.java
+++ app/src/main/java/com/example/libraconnect/StudentHomeFragment.java
@@ -1,41 +1,116 @@
 package com.example.libraconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-public class StudentHomeFragment extends Fragment implements View.OnClickListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class StudentHomeFragment extends Fragment {
+
+    private Button cancelborrowbookBtn, checkborrowbookBtn;
+    private CardView borrowbookCd;
+    private CardView readbookCd;
+
+    confirm_borrow confirmBorrow = new confirm_borrow();
+    private CardView confirmBorrowDialog;
+    private FirebaseAuth mAuth;
+    private TextView displayUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_student_home, container, false);
 
-        CardView borrowBookCardView = view.findViewById(R.id.borrowbookCd);
-        CardView readBookCardView = view.findViewById(R.id.readbookCd);
+        displayUser = view.findViewById(R.id.usernameTV);
+        borrowbookCd = view.findViewById(R.id.borrowbookCd);
+        readbookCd = view.findViewById(R.id.readbookCd);
+        confirmBorrowDialog = view.findViewById(R.id.confirmBorrowDialog);
+        mAuth = FirebaseAuth.getInstance();
 
-        borrowBookCardView.setOnClickListener(this);
-        readBookCardView.setOnClickListener(this);
 
+        // Move this line here
+        confirmBorrowDialog.setVisibility(View.INVISIBLE);
+
+        borrowbookCd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmBorrowDialog.setVisibility(View.VISIBLE);
+
+                cancelborrowbookBtn = view.findViewById(R.id.cancelborrowbookBtn);
+                checkborrowbookBtn = view.findViewById(R.id.checkborrowbookBtn);
+
+                cancelborrowbookBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        confirmBorrowDialog.setVisibility(View.INVISIBLE);
+                    }
+                });
+
+                checkborrowbookBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.student_frameLayout, confirmBorrow).commit();
+                    }
+                });
+            }
+        });
+
+        readbookCd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        displayUserName(view);
         return view;
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.borrowbookCd:
-                Navigation.findNavController(v).navigate(R.id.);
-                break;
-            case R.id.readbookCd:
-                Navigation.findNavController(v).navigate(R.id.);
-                break;
-        }
+    public void displayUserName(View view)
+    {
+        TextView displayUser = view.findViewById(R.id.usernameTV);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String uid = user.getUid();
+        DocumentReference userRef = db.collection("students").document(uid);
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // DocumentSnapshot contains the data
+                        String username = document.getString("name");
+
+                        // Use the username string as needed
+                        displayUser.setText(username);
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
     }
+
 }
